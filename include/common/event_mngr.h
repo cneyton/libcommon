45897d92ebdd43cdb102cdbf565d3b473b89b047
cne <c.neyton@nateo-healthcare.com>
2020-11-19 09:58:19 +0100

    [EVENT] add event manager
    Signed-off-by: cne <c.neyton@nateo-healthcare.com>

---
+++ include/common/event_mngr.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+#include <set>
+
+namespace common {
+
+template<typename EventType>
+class EventMngr
+{
+public:
+    void notify(EventType e)
+    {
+        std::lock_guard<std::mutex> lk(mutex_);
+        events_.insert(e);
+        cv_.notify_all();
+    }
+
+    void wait()
+    {
+        std::unique_lock<std::mutex> lk(mutex_);
+        cv_.wait(lk, [this]{return !events_.empty();});
+    }
+
+    void wait(EventType e)
+    {
+        std::unique_lock<std::mutex> lk(mutex_);
+        cv_.wait(lk, [this, e]{return (events_.find(e) != events_.end());});
+    }
+
+    bool erase(EventType e)
+    {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return events_.erase(e);
+    }
+
+    bool contain(EventType e)
+    {
+        std::lock_guard<std::mutex> lk(mutex_);
+        return !(events_.find(e) == events_.end());
+    }
+
+private:
+    std::set<EventType>     events_;
+    std::mutex              mutex_;
+    std::condition_variable cv_;
+};
+
+} /* namespace common */
+
